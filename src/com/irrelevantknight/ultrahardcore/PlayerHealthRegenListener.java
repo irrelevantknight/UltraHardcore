be0063220ccf9c2b3cd3ced51bda50f6f7321c95
irrelevantknight <irrelevantknight@gmail.com>
2013-05-09 08:11:29 -0400

    Changed code for preventing hunger regen. Not yet working

--- src/com/irrelevantknight/ultrahardcore/PlayerHealthRegenListener.java
+++ src/com/irrelevantknight/ultrahardcore/PlayerHealthRegenListener.java
@@ -1,18 +1,60 @@
 package com.irrelevantknight.ultrahardcore;
 
+import net.canarymod.api.entity.living.humanoid.Player;
 import net.canarymod.hook.HookHandler;
+import net.canarymod.hook.entity.PotionEffectAppliedHook;
 import net.canarymod.hook.player.HealthChangeHook;
 import net.canarymod.plugin.PluginListener;
 
 public class PlayerHealthRegenListener implements PluginListener
 {
-	//Don't allow players to regen health
+	
+	private UltraHardcore plugin;
+	
+	public PlayerHealthRegenListener(UltraHardcore instance)
+	{
+		plugin = instance;
+	}
+
+	//Prevent player from regenerating due to having enough hunger
 	@HookHandler
 	public void onPlayerHealthChange(HealthChangeHook hook)
 	{
-		if (hook.getNewValue() > hook.getOldValue())
+		int newHealth = hook.getNewValue();
+		int oldHealth = hook.getOldValue();
+		
+		//Not working properly at the moment
+		if (newHealth > oldHealth)
 		{
-			hook.setCanceled();
+			if (newHealth > hook.getPlayer().getMaxHealth())
+			{
+				hook.setCanceled();
+			}
+		}
+		else if (newHealth < oldHealth)
+		{
+			int healthDifference = oldHealth - newHealth;
+			hook.getPlayer().setMaxHealth(hook.getPlayer().getMaxHealth() - healthDifference);
 		}
 	}
+	
+	@HookHandler
+	public void onPotionEffectApplied(PotionEffectAppliedHook hook)
+	{
+		if (hook.getEntity() instanceof Player)
+		{
+			//If the potion is a regeneration potion
+			if (hook.getPotionEffect().getPotionID() == 10)
+			{
+				//Calculate the amount of hearts to add
+				int potionDuration = hook.getPotionEffect().getDuration();
+				int potionAmplifier = hook.getPotionEffect().getAmplifier();
+				int delayBetweenHealing = (int) (25 * Math.pow(0.5, potionAmplifier));
+				int heartsToAdd = potionDuration / delayBetweenHealing;
+				hook.getEntity().getPlayer().setMaxHealth(hook.getEntity().getPlayer().getMaxHealth() + heartsToAdd);
+			}
+		}
+	}
+	
+	
 }
